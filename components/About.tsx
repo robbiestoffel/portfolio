f1635b0e5671425f731b70bafdc7aa4582d4e229
robbiestoffel <robbiestoffel@gmail.com>
2024-09-22 11:54:48 -0400

    minor tweaks, burger needed for mobile

--- components/About.tsx
+++ components/About.tsx
@@ -2,32 +2,36 @@ import Image from "next/image";
 
 export default function About() {
 	return (
-		<div className="bg-brown-900 flex flex-row justify-around py-20 px-4 md:px-20" id="about">
+		<div
+			className="bg-brown-900 flex flex-row justify-around py-20 px-4 md:px-20"
+			id="about"
+		>
 			<Image
 				src="/profile.jpg"
 				height={600}
 				width={450}
-				className='h-600px w-450 m-10 border-2 border-stone-100 hidden xl:block'
+				className="h-600px w-450 m-10 border-2 border-stone-100 hidden xl:block"
 				alt="Picture of the Artist"
 			/>
-			<p className='text-xl md:p-10'>
+			<p className="text-xl md:p-10">
 				Robbie is from Mount Holly, North Carolina, just outside Charlotte. In
 				middle school, they took Visual Art three years in a row and Digital
 				Photography in eighth grade. When they entered high school, Robbie began
 				creating art through theater. In the last 4 years, they have played main
 				characters in seven productions on the stage and taken three theater
 				classes. In addition, they contribute to theatrical productions from off
-				stage, Robbie has been the lighting designer for two shows, one of which
-				they also were the stage manager. At the end of their sophomore year of
-				high school, Robbie was accepted into the North Carolina School of
-				Science and Math (NCSSM), where they continued to pursue their STEM and
-				artistic interests. At NCSSM, Robbie has taken an introductory visual
-				art class where they made exceptional growth learning to capture the
-				world around them in their art. Now a Senior in High School, Robbie was
-				accepted into the Advanced Studio Art class, where they are taking
-				new creative strides and building their own portfolio. Robbie believes
-				their art is meant to capture emotions that go unseen, and they strive
-				to achieve this in their art.
+				the stage, Robbie has been the lighting designer for two shows, one of
+				which they also were the stage manager. At the end of their sophomore
+				year of high school, Robbie was accepted into the North Carolina School
+				of Science and Math (NCSSM), where they continued to pursue their STEM
+				and artistic interests. At NCSSM, Robbie has taken an introductory
+				visual art class where they made exceptional growth learning to capture
+				the world around them in their art. Now a Senior in High School, Robbie
+				as been accepted into the Advanced Studio Art class, where they are
+				taking new creative strides and building their own portfolio. Influenced
+				by a variety of singer-songwriters such as Conan Gray, Lizzy McAlpine,
+				Gracie Abrams, Kevin Atwater, Mitski, and Billie Eilish, Robbie
+				strives to capture emotions that go unseen in their work.
 			</p>
 		</div>
 	);
--- components/Header.tsx
+++ components/Header.tsx
@@ -15,14 +15,14 @@ export default function Header() {
 	const links: Links[] = [
 		{ link: "/#about", label: "About" },
 		{ link: "/#contact", label: "Contact" },
-		{ link: "/art", label: "Art" },
+		{ link: "/art", label: "My Art" },
 	];
 
 	const items = links.map((link) => (
 		<Link
 			key={link.label}
 			href={link.link}
-			className="p-2 text-xl font-bold hover:text-rose-900"
+			className="p-3 text-lg sm:text-xl font-bold hover:text-rose-900"
 		>
 			{link.label}
 		</Link>
@@ -33,7 +33,7 @@ export default function Header() {
 		{ link: "/art/sketchbook", label: "Sketchbook" },
 		// { link: "/art/painting", label: "Photography" },
 		// { link: "/art/photography", label: "Photography" },
-		{ link: "/art/", label: "Art" },
+		{ link: "/art/", label: "My Art" },
 	];
 
 	const artItems = artLinks.map((link) => (
@@ -41,9 +41,9 @@ export default function Header() {
 			key={link.label}
 			href={link.link}
 			className={clsx(
-				"p-2 text-xl font-bold hover:text-rose-900",
+				"p-3 text-lg sm:text-xl font-bold hover:text-rose-900",
 				{
-					'text-brown-900 hover:text-rose-900 bg-stone-300 rounded-md': link.label === "Art" && pathname.slice(0, 4) === "/art"
+					'text-brown-900 hover:text-rose-900 bg-stone-300 rounded-md': link.label === "My Art" && pathname.slice(0, 4) === "/art"
 				}
 			)}
 		>
@@ -54,8 +54,8 @@ export default function Header() {
 	return (
 		<div className="h-18 w-screen fixed top-0 left-0 z-2 rounded-none bg-brown-900">
 			<div className="flex flex-rox justify-between h-full w-full p-5 items-center">
-				<Link href="/" className="text-2xl font-bold justify-center">
-					Robbie <em className="text-xl">(they/them)</em>
+				<Link href="/" className="text-xl sm:text-2xl font-bold justify-center">
+					Robbie <em className="hidden sm:inline sm:text-xl">(they/them)</em>
 				</Link>
 				<div
 					className={clsx("", {

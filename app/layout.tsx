bd87162fdc576c8d74877fbf4c6e6b1e7ef7900f
robbiestoffel <robbiestoffel@gmail.com>
2024-11-20 20:14:03 -0500

    Revert "statement update"
    This reverts commit 7c51e57b1aec6003582e08a7d939e1e25b1592fa.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,14 +3,13 @@ import { Sofadi_One } from "next/font/google";
 import { Ubuntu } from "next/font/google";
 import { Kalam } from "next/font/google";
 import "./globals.css";
-import { MantineProvider, ScrollArea } from "@mantine/core";
+import { MantineProvider } from "@mantine/core";
 import "@mantine/core/styles.css";
 import Header from "../components/Header";
-import { NavbarNested } from "../components/NavBar/NavbarNested";
 
 const sofadi = Sofadi_One({ subsets: ["latin"], weight: "400" });
-// const ubuntu = Ubuntu({ subsets: ["latin"], weight: "400" });
-// const kalam = Kalam({ subsets: ["latin"], weight: "400" });
+const ubuntu = Ubuntu({ subsets: ["latin"], weight: "400" });
+const kalam = Kalam({ subsets: ["latin"], weight: "400" });
 
 export const metadata: Metadata = {
 	title: "Robbie - Portfolio",
@@ -26,11 +25,9 @@ export default function RootLayout({
 		<html lang="en">
 			<body className={sofadi.className}>
 				<MantineProvider>
-					<div className="bg-rose-950 text-stone-100 flex flex-row">
-						<div>
-							<NavbarNested />
-						</div>
-						<ScrollArea>{children}</ScrollArea>
+					<div className='bg-rose-950 text-stone-100'>
+						<Header />
+						{children}
 					</div>
 				</MantineProvider>
 			</body>

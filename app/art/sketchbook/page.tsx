d96614f701d29b76c368cf11bf26561f25359238
robbiestoffel <robbiestoffel@gmail.com>
2024-11-20 20:12:54 -0500

    Revert "moved wrist to top"
    This reverts commit 38f07ca1a08048f8414275bb72e4b4423c635e4b.

--- app/art/sketchbook/page.tsx
+++ app/art/sketchbook/page.tsx
@@ -3,15 +3,6 @@ import Display from "../../../components/Art/display"
 export default function Page() {
   return(
     <div className='pt-24 bg-brown-950 place-content-around'>
-			<Display
-				title="Wrist"
-				medium="graphite on paper"
-				size="size coming soon"
-				year="2024"
-				src="/wrist.jpg"
-        portrait={true}
-				alt="Sketchbook Drawing of my wrist"
-			/>
       <Display
 				title="Lil Penguin"
 				medium="graphite on paper"
@@ -38,6 +29,15 @@ export default function Page() {
 				src="/lilpenguin(process).jpg"
         portrait={true}
 				alt="Lil Penguin Process Sketchbook Drawing"
+			/>
+      <Display
+				title="Wrist"
+				medium="graphite on paper"
+				size="size coming soon"
+				year="2024"
+				src="/wrist.jpg"
+        portrait={true}
+				alt="Sketchbook Drawing of my wrist"
 			/>
     </div>
   )
